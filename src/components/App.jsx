8d6af5fa18bf8cccedf055c1bd09aee30aa0200b
Petr Mazka <petr.mazka@gmail.com>
2022-07-19 22:59:44 +0300

    1
    1

---
+++ src/components/App.jsx
@@ -0,0 +1,19 @@
+import Button from "./Button";
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101'
+      }}
+    >
+
+      
+    </div>
+  );
+};
